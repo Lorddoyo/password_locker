02c6013251ce5a0bbcb3db3e92812c919db6c4bd
Lorddoyo <johnkojo7777@gmail.com>
2019-05-08 15:54:47 +0300

    created a claass for user and ceredintials

---
+++ user.py
@@ -0,0 +1,14 @@
+class User:
+    users_list = []
+    '''
+    making 
+    '''
+    def __init__(self, username, email, password):
+        '''
+
+        '''
+        self.uname = username
+        self.email = email
+        self.pword = password
+
+        
