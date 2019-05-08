d49ad5dc69dd8f6fccfc84e56b0eceb390686e12
Lorddoyo <johnkojo7777@gmail.com>
2019-05-08 16:39:16 +0300

    created user function

--- user.py
+++ user.py
@@ -1,14 +1,21 @@
 class User:
     users_list = []
     '''
-    making 
+    Class that generates new instances of contacts 
     '''
     def __init__(self, username, email, password):
         '''
-
+        function to intialize class
         '''
         self.uname = username
         self.email = email
         self.pword = password
 
-        
+
+    def createUser(self, uname, email, pword):
+        '''
+        Function to create new user
+
+        '''
+        new_user = User( uname, email, pword)
+        return new_user
