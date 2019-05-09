57e7786d49028c09efd42db112edc79c4f485939
Lorddoyo <johnkojo7777@gmail.com>
2019-05-09 10:57:25 +0300

    create test_save _user test case

--- user_test.py
+++ user_test.py
@@ -5,7 +5,7 @@ import pyperclip
 
 class TestUser(unittest.TestCase):
     '''
-    Test clasnew_contactnew_contacts defines test cases for the user class behaviours.
+    Test clasnew_usernew_user defines test cases for the user class behaviours.
 
     Args:
     unittest.Testcase: Testcase class that helps in creating test cases
@@ -15,6 +15,7 @@ class TestUser(unittest.TestCase):
         '''
         '''
         self.new_user =User('Lorddoyo', 'john@gmail.com', 'damnn')
+
     def test_init(self):
         '''
         test_init test case to test if the object is intialized properly
@@ -22,4 +23,18 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.uname, 'Lorddoyo') 
         self.assertEqual(self.new_user.email, 'john@gmail.com')
         self.assertEqual(self.new_user.pword, 'damnn')
-        
+    def tearDown(self):
+        User.users_list=[]
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+    
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the contact object is saved into the user users_list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.users_list),1)
+
+    
+    
