ce3b72fa74260dfced8622b9d27c956341f11915
Lorddoyo <johnkojo7777@gmail.com>
2019-05-09 10:46:52 +0300

    instanct testcase class

--- user_test.py
+++ user_test.py
@@ -1,3 +1,25 @@
 import unittest
 from user import User
 import pyperclip
+
+
+class TestUser(unittest.TestCase):
+    '''
+    Test clasnew_contactnew_contacts defines test cases for the user class behaviours.
+
+    Args:
+    unittest.Testcase: Testcase class that helps in creating test cases
+    '''
+
+    def setUp(self):
+        '''
+        '''
+        self.new_user =User('Lorddoyo', 'john@gmail.com', 'damnn')
+    def test_init(self):
+        '''
+        test_init test case to test if the object is intialized properly
+        '''
+        self.assertEqual(self.new_user.uname, 'Lorddoyo') 
+        self.assertEqual(self.new_user.email, 'john@gmail.com')
+        self.assertEqual(self.new_user.pword, 'damnn')
+        
