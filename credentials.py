db3e72353fdf90c605c73d00c2bf5187e8f81e56
Lorddoyo <johnkojo7777@gmail.com>
2019-05-12 11:39:32 +0300

    removed the find all user's function

--- credentials.py
+++ credentials.py
@@ -14,24 +14,17 @@ class Credentials:
         self.account_email = account_email
         self.account_password = account_password
 
+    def delete_credentials(self):
 
-    # def display_all_accounts(self):
-    #      '''
-    #     display_all_accounts method returns the credentials list
-    #     '''
-
-    #     return Credentials.credentials_list
-
-   
-    def find_by_account(self, name):
         '''
-        find_by_account method takes a searched account name and returns account credentials of that account.
-        Args:
-            account: Account name to search for
-        Returns :
-            Credentials of the user account that matches the searched account.
+        delete_credentials method deletes a saved user from the user_list
+        '''
+        Credentials.delete_credentials(self)
+
+    def add_credentials(self):
+        '''
         '''
 
-        for account in self.credentials_list:
-            if account.account_name == name:
-                return account
+        Credentials.add_credentials(self)
+
+
